fbff2c9e218c6cff0dd70765c3c0781333db3777
chrislin <lemon0417@gmail.com>
2020-03-25 03:51:37 +0800

    Initial commit

---
+++ src/test/java/com/hexschool/keybr/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hexschool.keybr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
